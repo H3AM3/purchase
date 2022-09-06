aedc20563cab5c39fcad7cfdb670e39106220c6f
H3AM3 <dnlsehdn2@naver.com>
2022-09-06 18:07:13 +0900

    interceptor permission code bug fix

--- purchase/src/main/java/com/docmall/interceptor/AdminInterceptor.java
+++ purchase/src/main/java/com/docmall/interceptor/AdminInterceptor.java
@@ -2,9 +2,12 @@ package com.docmall.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.docmall.domain.MemberVO;
 
 import lombok.extern.log4j.Log4j;
 @Log4j
@@ -14,23 +17,25 @@ public class AdminInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		boolean result = false;
-		if(isAdmin(request)) {
+		HttpSession session = request.getSession();
+		if(isAdmin(session)) {
 			result = true;
 		}else {
-			log.info("권한없는 사용자의 접근");
 			response.sendError(400, "permission error");
 		}
 		return result;
 	}
 
-	private boolean isAdmin(HttpServletRequest request) {
+	private boolean isAdmin(HttpSession session) {
 		boolean result = false;
-		log.info(request.getAttribute("mem_level"));
-		if(request.getAttribute("mem_level")=="1") {
+		MemberVO user = (MemberVO) session.getAttribute("loginStatus");
+		System.out.println(Integer.toString(user.getMem_level()));
+		if(Integer.toString(user.getMem_level()).equals("1")) {
 			result = true;
 		}
 		return result;
 	}
+
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
